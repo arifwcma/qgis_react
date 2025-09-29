7bafef8ba5e64ce491b54a65363caf0d7980b98f
arif.rahman <arif.rahman@wcma.vic.gov.au>
2025-09-29 15:20:08 +1000

    done

--- src/Raster1.jsx
+++ src/Raster1.jsx
@@ -1,26 +1,20 @@
-import React, { useEffect } from 'react'
-import L from 'leaflet'
+import { MapContainer, TileLayer, WMSTileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
 export default function Raster1() {
-    useEffect(() => {
-        const map = L.map('map', {
-            center: [52.21640, 4.37645],
-            zoom: 14
-        })
-
-        L.tileLayer.wms(
-            'http://testpozi.online/cgi-bin/qgis_mapserv.fcgi?MAP=/var/www/qgis_projects/qs_test_prizen/qs_test_prizen.qgs&',
-            {
-                layers: 'sample',
-                format: 'image/png',
-                transparent: true,
-                version: '1.3.0'
-            }
-        ).addTo(map)
-
-        return () => map.remove()
-    }, [])
-
-    return <div id="map" style={{ height: '100vh', width: '100%' }} />
+    return (
+        <MapContainer center={[52.21640, 4.37645]} zoom={14} style={{ height: '100vh', width: '100vw' }}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <WMSTileLayer
+                url="http://testpozi.online/cgi-bin/qgis_mapserv.fcgi"
+                params={{
+                    MAP: '/var/www/qgis_projects/qs_test_prizen/qs_test_prizen.qgs',
+                    layers: 'sample',
+                    format: 'image/png',
+                    transparent: true,
+                    version: '1.3.0'
+                }}
+            />
+        </MapContainer>
+    )
 }
