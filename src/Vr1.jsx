07cd99f7c763ad9e4364f034a687ac46445f8ba0
arif.rahman <arif.rahman@wcma.vic.gov.au>
2025-09-30 13:16:26 +1000

    done

--- src/Vr1.jsx
+++ src/Vr1.jsx
@@ -1,12 +1,20 @@
-import { MapContainer, TileLayer, WMSTileLayer } from 'react-leaflet'
+import { useEffect, useState } from 'react'
+import { MapContainer, TileLayer, GeoJSON, WMSTileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-import CountriesLayer from './CountriesLayer'
+import axios from 'axios'
 
 export default function Vr1() {
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        axios.get('http://testpozi.online/cgi-bin/qgis_mapserv.fcgi?MAP=/var/www/qgis_projects/qs_test_prizen/qs_test_prizen.qgs&SERVICE=WFS&REQUEST=GetFeature&VERSION=1.1.0&TYPENAME=countries&OUTPUTFORMAT=application/json')
+            .then(res => setData(res.data))
+    }, [])
+
     return (
-        <MapContainer center={[52.21640, 4.37645]} zoom={14}  style={{ height: '100vh', width: '100vw' }}>
+        <MapContainer center={[52.21640, 4.37645]} zoom={12} style={{ height: '100vh', width: '100vw' }}>
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <CountriesLayer />
+            {data && <GeoJSON data={data} />}
             <WMSTileLayer
                 url="http://testpozi.online/cgi-bin/qgis_mapserv.fcgi"
                 params={{
