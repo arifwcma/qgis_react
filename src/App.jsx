7430148a52ebd3bdad1d1019c506e7eb4675a4da
arif.rahman <arif.rahman@wcma.vic.gov.au>
2025-09-30 13:10:04 +1000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MapView from './MapView.jsx'
 import Raster1 from './Raster1.jsx'
 import Info from './Info'
+import Vr1 from './Vr1.jsx'
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Route path="/" element={<MapView />} />
                 <Route path="/raster1" element={<Raster1 />} />
                 <Route path="/info" element={<Info />} />
+                <Route path="/vr1" element={<Vr1 />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/Vr1.jsx
@@ -0,0 +1,22 @@
+import { MapContainer, TileLayer, WMSTileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import CountriesLayer from './CountriesLayer'
+
+export default function Vr1() {
+    return (
+        <MapContainer center={[52.21640, 4.37645]} zoom={14}  style={{ height: '100vh', width: '100vw' }}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <CountriesLayer />
+            <WMSTileLayer
+                url="http://testpozi.online/cgi-bin/qgis_mapserv.fcgi"
+                params={{
+                    MAP: '/var/www/qgis_projects/qs_test_prizen/qs_test_prizen.qgs',
+                    layers: 'sample',
+                    format: 'image/png',
+                    transparent: true,
+                    version: '1.3.0'
+                }}
+            />
+        </MapContainer>
+    )
+}
